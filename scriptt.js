016429f298b36f3fdd73e9c073df71756e3feeae
Kanika rajput <rajputkanika874@gmail.com>
2026-04-24 00:19:23 +0530

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,5 @@
+console.log("hello kanika");
+//let box=document.getElementsByClassName("box");
+//console.log(box);
+//box[2].style.backgroundColor="red";
+document.getElementById("red").style.backgroundColor="red";
